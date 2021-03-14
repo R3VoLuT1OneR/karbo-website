7ee5f4355580c567f90c67d7224aed7e1035604f
R3VoLuT1OneR <R3VoLuT1OneR@users.noreply.github.com>
2021-03-14 22:38:16 +0000

    deploy: 6dbe693b202e2dd7ca98ad7beb4c20f293661921

--- _nuxt/static/1615152133/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkarbo-website\u002F_nuxt\u002Fstatic\u002F1615152133",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"karbo-website\u002F",assetsPath:"karbo-website\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3d7fb3fd"}}}}(null));
