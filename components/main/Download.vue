c413cb4801bb7376d374e9906be909c7523863af
Pavel Z <r3volut1oner@gmail.com>
2021-03-17 00:35:24 +0200

    Download links

--- components/main/Download.vue
+++ components/main/Download.vue
@@ -17,6 +17,7 @@
           :href="href"
           :class="{ 'mr-6': wallets.length - 1 !== i }"
           class="text-black mr-6 opacity-50 hover:opacity-100"
+          target="_blank"
         >
           <SvgIcon :name="`wallets/${icon}`" class="w-6 h-6" />
         </a>
@@ -30,32 +31,85 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import { Octokit } from '@octokit/rest'
+const octokit = new Octokit()
+
+/* eslint-disable camelcase */
+interface ReleaseAsset {
+  name: string
+  browser_download_url: string
+}
+/* eslint-enable camelcase */
 
 @Component
 export default class Download extends Vue {
+  assets: ReleaseAsset[] = []
+
+  async fetch() {
+    const releases = await octokit.repos.listReleases({
+      owner: 'Karbovanets',
+      repo: 'karbo',
+    })
+      .then(rsp => rsp.data)
+
+    this.assets = releases[0].assets
+  }
+
   get wallets() {
-    return [
+    const assetsToOS = this.mapAssetsToOS()
+    let walletsList = [
       {
+        os: 'windows',
         icon: 'windows',
         href: '',
       },
       {
+        os: 'ubuntu',
         icon: 'ubuntu',
         href: '',
       },
       {
+        os: 'macOS',
         icon: 'apple',
         href: '',
       },
       {
         icon: 'android',
-        href: '',
+        href: 'https://play.google.com/store/apps/details?id=org.karbo.karbon',
       },
       {
         icon: 'sources',
-        href: '',
+        href: 'https://github.com/Karbovanets/karbo/releases',
       },
     ]
+
+    walletsList = walletsList.map((w) => {
+      if (w.os && assetsToOS[w.os]) {
+        w.href = assetsToOS[w.os].browser_download_url
+      }
+
+      return w
+    })
+
+    return walletsList
+  }
+
+  private mapAssetsToOS(): Record<string, ReleaseAsset> {
+    const mapNameRegexp: Record<string, RegExp> = {
+      windows: /^Karbo-cli-win64/,
+      macOS: /^Karbo-cli-macOS/,
+      ubuntu: /^Karbo-cli-ubuntu20.04/,
+    }
+
+    return this.assets.reduce((acc, asset) => {
+      for (const [os, regexp] of Object.entries(mapNameRegexp)) {
+        if (regexp.test(asset.name)) {
+          return { ...acc, [os]: asset }
+        }
+      }
+
+      return acc
+    }, {})
   }
 }
 </script>
