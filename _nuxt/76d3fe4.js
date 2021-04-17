604f5538f77b5ce5a62f62dd25d41018e9c49c8c
R3VoLuT1OneR <R3VoLuT1OneR@users.noreply.github.com>
2021-04-17 16:15:40 +0000

    deploy: 9fc7f2880d2f17860afbb3d806015290f24bf58c

---
+++ _nuxt/76d3fe4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{377:function(o,n,w){"use strict";w.r(n),n.default={About:"О Карбо",Download:"Скачать",Usage:"Успользование",Community:"Сообщество"}}}]);
--- _nuxt/bcf2e86.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{371:function(o,n,w){"use strict";w.r(n),n.default={About:"О Карбо",Download:"Завантажити",Usage:"Використання",Community:"Спільнота"}}}]);
